9214585a57d48d4e693d664be4fa2e13749d7acf
Quentin Bahers <quentin.bahers@gmail.com>
2015-05-08 22:00:31 +0100

    Solve TreeHeight

---
+++ 99-future-training/TreeHeight.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(Tree T) {
+        if (T == null) {
+        	return -1;
+        }
+        else {
+            return 1 + Math.max(solution(T.l), solution(T.r));
+        }
+    }
+    
+}
